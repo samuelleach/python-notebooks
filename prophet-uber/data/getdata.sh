a2f5cddb89196617ff47324b72dd8fb2491a5cec
samuelleach <samuel.leach@gmail.com>
2017-04-17 19:35:49 +0100

    Initial commit

---
+++ prophet-uber/data/getdata.sh
@@ -0,0 +1,3 @@
+wget https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/uber-raw-data-janjune-15.csv.zip
+wget https://github.com/fivethirtyeight/uber-tlc-foil-response/raw/master/uber-trip-data/taxi-zone-lookup.csv
+unzip uber-raw-data-janjune-15.csv.zip
